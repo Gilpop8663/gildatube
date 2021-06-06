f07fa5d055a5d8e1e001db557bc5f5e2d6f4c20f
GilPOP <wolfye@naver.com>
2021-06-07 06:18:24 +0900

    site Join & password bcrypt using

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/gildatube" ,{useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect("mongodb://127.0.0.1:27017/gildatube" ,{useNewUrlParser: true,useUnifiedTopology: true ,useCreateIndex:true,});
 
 const db = mongoose.connection;
 
